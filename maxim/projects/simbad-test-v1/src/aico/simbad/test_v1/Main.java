81fa925808ea76c67f091eb717f682249c6d642e
Maxim Pestun <max.pestun@gmail.com>
2012-12-28 12:43:18 +0400

    some updated on another computer
    and adding jar files to repo submittion list

--- maxim/projects/simbad-test-v1/src/aico/simbad/test_v1/Main.java
+++ maxim/projects/simbad-test-v1/src/aico/simbad/test_v1/Main.java
@@ -4,7 +4,7 @@ import simbad.gui.Simbad;
 public class Main {
 
 	public static void main(String[] args) {
-		Simbad frame = new Simbad(new MyEnv() ,false);
+		final Simbad frame = new Simbad(new MyEnv() ,false);
 	}
 
 }
