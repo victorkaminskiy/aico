b2e5c511eef7be328bece310980ea5d99a9d1778
Maxim Pestun <max.pestun@gmail.com>
2012-12-28 13:16:11 +0400

    attached sources and updated level to match real

--- maxim/projects/simbad-test-v1/src/aico/simbad/test_v1/MyEnv.java
+++ maxim/projects/simbad-test-v1/src/aico/simbad/test_v1/MyEnv.java
@@ -5,7 +5,23 @@ import javax.vecmath.Vector3d;
 
 public class MyEnv extends EnvironmentDescription {
 	public MyEnv(){
-        add(new Arch(new Vector3d(3,0,-3),this));
-        add(new MyRobot(new Vector3d(0, 0, 0),"my robot"));
+		// Square walls
+		Wall w1 = new Wall(new Vector3d(9, 0, 0), 18, 2, this);
+        w1.rotate90(1);
+        add(w1);
+        Wall w2 = new Wall(new Vector3d(-9, 0, 0), 18, 2, this);
+        w2.rotate90(1);
+        add(w2);
+        Wall w3 = new Wall(new Vector3d(0, 0, 9), 18, 2, this);
+        add(w3);
+        Wall w4 = new Wall(new Vector3d(0, 0, -9), 18, 2, this);
+        add(w4);
+        
+        // Middle wall
+        Wall w5 = new Wall(new Vector3d(0, 0, -2), 14, 2, this);
+        w5.rotate90(1);
+        add(w5);
+        
+        add(new MyRobot(new Vector3d(5, 0, -5),"Aico Robot"));
     }
 }
