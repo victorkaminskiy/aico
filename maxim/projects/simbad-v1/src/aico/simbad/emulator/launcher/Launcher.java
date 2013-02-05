b89236956c5ca91c48b2a9f4fd22294bb96331f3
Maxim Pestun <max.pestun@gmail.com>
2013-02-05 19:46:41 +0400

    print debug java.library.path

--- maxim/projects/simbad-v1/src/aico/simbad/emulator/launcher/Launcher.java
+++ maxim/projects/simbad-v1/src/aico/simbad/emulator/launcher/Launcher.java
@@ -6,6 +6,7 @@ import simbad.gui.Simbad;
 public class Launcher {
 
 	public static void main(String[] args) {
+		System.out.println("LD Library Path: " + System.getProperty("java.library.path"));
 		final Simbad frame = new Simbad(new Environment() ,false);
 	}
 	
